a2bd55aded60bb3ca31d47193e40d0f26887992b
Klif <sang.wicklife@student.moringaschool.com>
2023-12-18 00:48:58 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'klif';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toLocaleUpperCase();
+}
+
+function setBestCustomer() {
+    var bestCustomer = `'not bob'`;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
